6b46c9eceec21fc2eb0d266489a586408b57db91
collins njau <collinsnjau39@gmail.com>
2019-05-13 20:02:32 +0300

    Store configs - add file to manage key and secret - add inquirer to interact with the user

--- bin/twine.js
+++ bin/twine.js
@@ -1,3 +1,11 @@
 #! /usr/bin/env node
 
-console.log('Hello');
+const CredentialsManager = require('../lib/credentialManager.js');
+
+const main = async () => {
+  const credentials = new CredentialsManager('twine');
+  const [key, secret] = await credentials.getKeyAndSecret();
+  console.log(key, secret);
+};
+
+main().catch(console.error);
